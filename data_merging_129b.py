1811a16a83c4b58411a644b10456e9fc910caa2b
Apoorv Goyal <apoorvelous@gmail.com>
2020-10-23 15:44:55 +0530

    first commit

---
+++ data_merging_129b.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 17:43:26 2020
+
+@author: Vishal
+"""
+import csv
+import pandas as pd
+
+file1 = 'bright_stars.csv'
+file2 = 'unit_converted_stars.csv'
+
+d1 = []
+d2 = []
+with open(file1,'r',encoding='utf8') as f:
+    csv_reader =csv.reader(f)
+    
+    for i in csv_reader:
+        d1.append(i)
+        
+with open(file2,'r',encoding='utf8') as f:
+    csv_reader = csv.reader(f)
+    
+    for i in csv_reader:
+        d2.append(i)
+
+h1 = d1[0]
+h2 = d2[0]
+
+p_d1 = d1[1:]
+p_d2 = d2[1:]
+
+h = h1+h2
+
+p_d =[]
+
+for i in p_d1:
+    p_d.append(i)
+for j in p_d2:
+    p_d.append(j)
+with open("total_stars.csv",'w',encoding='utf8') as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(h)   
+    csvwriter.writerows(p_d)
+    
+df = pd.read_csv('total_stars.csv')
+df.tail(8)
